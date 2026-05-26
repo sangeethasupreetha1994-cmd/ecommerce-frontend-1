c11f05a6084b5dec5cef254a3c4f0dbd1eb89386
sangeethasupreetha1994-cmd <sangeethasupreetha1994@gmail.com>
2026-05-26 14:41:56 +0530

    updated

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "https://ecommerce-backend-1-9qv0.onrender.com"
+    baseURL: "https://ecommerce-backend-1-9qv0.onrender.com/api/v1/",
 });
 
 export default axiosInstance;     
